197cfa2f08167c5418b26e080f520c6e50510437
karan1149 <karan1149@gmail.com>
2015-12-29 02:20:06 -0500

    Implemented autoupdated sessions to track form data.

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,3 +1,27 @@
+/**
+airport = null;
+direction = null;
+  Meteor.setInterval(function(){
+    airport = new ReactiveVar();
+    direction = new ReactiveVar();
+    airport.set($('[name="airport"]').val());
+    direction.set($('[name="direction"]').val());
+  //   if (airport === "--" || direction === "--") self.$('.reveal').hide();
+}, 750)
+**/
+
+Session.set('airports', '--');
+Session.set('direction', '--');
+
+Meteor.setInterval(function(){
+  Session.set('airport',$('[name="airport"]').val())
+  Session.set('direction', $('[name="direction"]').val())
+}, 500);
+
+
 Template.home.helpers({
-  revealAfterAirport: function() { return ($('[name="direction"]').val() === "--" || $('[name="airport"]').val() === "--" ? "hide" : "")}
+ revealAfterAirport: function() { return (Session.get('airport') === "--" || Session.get('direction') === "--")? "hide" : ""},
+ antiDirection: function() {
+   return (Session.get('direction') === "to") ? "from" : "to";
+ }
 });
