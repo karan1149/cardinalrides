66415e73ff42d77cf87a10f4fa5e4ffb215e393f
karan1149 <karan1149@gmail.com>
2015-12-30 11:42:06 -0500

    Added automatic pacific time, factoring in daylight savings.

--- client/templates/submitted.js
+++ client/templates/submitted.js
@@ -5,5 +5,10 @@ Template.submitted.helpers({
   soon: function() {
     var date = new Date();
     return (this.time.getTime() - date.getTime() < 1000 * 3600 * 13);
+  },
+  pacificTime: function() {
+    var difference = getPacificOffset()*60 - this.time.getTimezoneOffset()*-1;
+    var newDate = new Date(this.time.getTime() + difference*60*1000);
+    return newDate.toString().substring(0,16) + getFormattedTime(newDate.toString().substring(16,21));
   }
 });
--- server/matchchecker.js
+++ server/matchchecker.js
@@ -9,16 +9,15 @@ Meteor.setInterval(function(){
     flight = upcomingFlights[i];
     var otherFlights = _.reject(Flights.find().fetch(), function(element) { return flight === element});
     otherFlights = _.filter(otherFlights, function(element) { return Math.abs(flight.time.getTime() - element.time.getTime()) < 1000*3600*leeway });
-    // Flights.update(flight._id, {$set: {matched:true}});
+    Flights.update(flight._id, {$set: {matched:true}});
     otherFlights = _.sortBy(otherFlights, function(otherFlight) {
       return Math.abs(otherFlight.time.getTime()-flight.time.getTime())
     });
-    /**
     Mailer.send({
       to: flight.email,
       subject: "Cardinal Rides Matches for Your " + flight.time.toString().substring(0,3) + ". Flight",
       template: 'matches',
       data: {matched: flight, other: otherFlights}
-    }); **/
+    });
   }
 }, 1000*1*3600);
