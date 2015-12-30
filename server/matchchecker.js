70aeb9a509cfdfd935aef57bf502690bda270e74
karan1149 <karan1149@gmail.com>
2015-12-30 02:25:24 -0500

    Created server matchChecker.
    When it is running, server checks for unmatched flights coming up and
    looks for flights to match, returning a sorted array to be sent using
    the matches template.

---
+++ server/matchchecker.js
@@ -0,0 +1,24 @@
+leeway = 3;
+
+Meteor.setInterval(function(){
+  date = new Date();
+  var upcomingFlights = _.filter(Flights.find({matched : {$ne: true}}).fetch(), function(flight){
+    return flight.time.getTime() < date.getTime() + 12*1000*3600;
+  });
+  for (i=0; i<upcomingFlights.length; i++) {
+    flight = upcomingFlights[i];
+    var otherFlights = _.reject(Flights.find().fetch(), function(element) { return flight === element});
+    otherFlights = _.filter(otherFlights, function(element) { return Math.abs(flight.time.getTime() - element.time.getTime()) < 1000*3600*leeway });
+    // Flights.update(flight._id, {$set: {matched:true}});
+    otherFlights = _.sortBy(otherFlights, function(otherFlight) {
+      return Math.abs(otherFlight.time.getTime()-flight.time.getTime())
+    });
+    /**
+    Mailer.send({
+      to: flight.email,
+      subject: "Cardinal Rides Matches for Your " + flight.time.toString().substring(0,3) + ". Flight",
+      template: 'matches',
+      data: {matched: flight, other: otherFlights}
+    }); **/
+  }
+}, 1000*1*3600);
